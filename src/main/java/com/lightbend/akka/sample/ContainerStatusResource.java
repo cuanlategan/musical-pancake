2428b7e3bc11a25cac0d547173ab5457f6b4533b
Cuan Lategan <cuanlategan@gmail.com>
2018-04-19 23:05:14 +0200

    add beginnings of rest client

---
+++ src/main/java/com/lightbend/akka/sample/ContainerStatusResource.java
@@ -0,0 +1,12 @@
+package com.lightbend.akka.sample;
+
+public class ContainerStatusResource {
+  private String status;
+
+  public ContainerStatusResource() {}
+
+  public String getStatus() {
+    return status;
+  }
+
+}
